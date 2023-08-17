5f32dd94752078e8227ace69f506bb8cd7d4c0b1
cristiandgarzon <138620741+cristiandgarzon@users.noreply.github.com>
2023-08-17 15:59:57 -0500

    añadiendo el footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,19 @@ import { useState } from 'react'
 import './App.css'
 import Header from './components/header/Header'
 import Body from './components/body/Body'
+import Footer from './components/footer/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <body>
+      <body className='container'> 
       <Header></Header>
       <div>
-      <Body>
-        
-      </Body>
-      
+      <Body></Body>
       </div>
-      
-      
+      <Footer></Footer>
       </body>
     </>
   )

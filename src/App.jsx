f3556dc988b8391553bb9193142062fae5081594
Jose Tucci <tucci.jose023@gmail.com>
2023-08-17 14:38:52 -0400

    Mi Primer commit añadiendo el React

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react';
+import Nav from './components/nav/Nav';
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div>
+      <Nav />
+      <h1>Contenido de la página</h1>
+      <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum, illo repellendus a ut unde dignissimos consequuntur, commodi quidem velit numquam eaque perspiciatis ad non impedit molestias, dicta dolor aspernatur laudantium.
       </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;

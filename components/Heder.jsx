fbe0026f03190f3abf0703aa521f6fda1e917f48
jonatan <framan2403@gmail.com>
2023-09-14 16:58:11 -0500

    encabezado con logo

---
+++ components/Heder.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Heder() {
+  return (
+    <div>
+        <h1>
+            <img src={logo} alt="logo ded la pagina" />
+        </h1>
+    </div>
+  )
+}
+
+export default Heder

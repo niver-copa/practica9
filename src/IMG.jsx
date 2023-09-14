62415c8d67051712c1ffe179e6cf050e57f884bc
eneging <edwarrejasreyes@gmail.com>
2023-09-14 17:51:49 -0500

    otro compoente imagen

--- src/IMG.jsx
+++ src/IMG.jsx
@@ -1,8 +1,9 @@
 import './App.css'
-
+import Img2 from './Img2'
 function IMG() {
   return (
-    <main><img src="https://okdiario.com/img/2023/04/10/-es-malo-que-los-gatos-coman-moscas-y-mosquitos-635x358.jpg"></img></main>
+    <main><img src="https://okdiario.com/img/2023/04/10/-es-malo-que-los-gatos-coman-moscas-y-mosquitos-635x358.jpg"></img>
+    <Img2></Img2></main>
   )
 }
 
---
+++ src/Img2.jsx
@@ -0,0 +1,9 @@
+
+
+function Img2() {
+  return (
+    <div><img src="https://elcomercio.pe/resizer/58mL4REVvTygv4cZdgtx460-Kes=/580x330/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/RVWSBX5OTRHK3DPV232FDV7ORM.jpg" alt="gato" /></div>
+  )
+}
+
+export default Img2

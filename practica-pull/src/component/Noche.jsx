0e5b1b1ad7acabb1a358730a4e789a298bf42e81
freirediego <diegofg23@outlook.es>
2023-09-14 17:06:29 -0500

    Agrego 3 componentes

---
+++ practica-pull/src/component/Noche.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Noche() {
+  return (
+    <div>
+      <h3>Son las 20 pm, ya es de noche!</h3>
+    </div>
+  );
+}
+
+export default Noche;
---
+++ practica-pull/src/component/Tarde.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Tarde() {
+  return (
+    <div>
+      <h3>Son las 14 pm, es de tarde!</h3>
+    </div>
+  );
+}
+
+export default Tarde;

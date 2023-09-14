874adff626aee3cc402ea7d4bb3b374a62358d27
jonatan <framan2403@gmail.com>
2023-09-14 17:17:17 -0500

    contenido de la pagina

---
+++ components/body.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function body() {
+  return (
+    <div>
+        <body>
+            <img src={iglesia} alt="" />
+        </body>
+        <p>
+            <h3>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. A dolorem eum impedit facere incidunt doloribus facilis officia deserunt porro, corrupti, iste ea ullam temporibus quae? Distinctio illum quia atque nemo?*10
+            </h3>
+        </p>
+    </div>
+  )
+}
+
+export default body

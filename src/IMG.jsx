f62bc7f6c02b68679cd4f442f58c4d7772f2f1e6
eneging <edwarrejasreyes@gmail.com>
2023-09-14 17:36:28 -0500

    segundo componente

---
+++ src/IMG.jsx
@@ -0,0 +1,9 @@
+import './App.css'
+
+function IMG() {
+  return (
+    <main><img src="https://okdiario.com/img/2023/04/10/-es-malo-que-los-gatos-coman-moscas-y-mosquitos-635x358.jpg"></img></main>
+  )
+}
+
+export default IMG

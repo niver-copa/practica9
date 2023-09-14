fe3423c464ff6d7d2926a5493089687b99991297
Michael Ojeda <ojedamichael62@gmail.com>
2023-09-14 19:35:26 -0300

    componente de resta

---
+++ componentes/resta/Resta.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Resta() {
+    const resta = () => {
+        4 - 4
+    } 
+  return (
+    <div>
+        <p>este es el ejemplo de como se resta en un componente y se muestra de que 4-4 = {resta}</p>
+    </div>
+  )
+}
+
+export default Resta

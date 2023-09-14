24a8b7d75dda728915df95b00215ddf345b336c3
teobaldo <teobaldo.navarro1@gmail.com>
2023-09-14 17:18:38 -0500

    tercer commit

--- components/Component1.jsx
+++ components/Component1.jsx
@@ -3,7 +3,12 @@ import "./Component1.css"
 
 function Component1() {
   return (
-    <div className='prueba'>Component1</div>
+    <div>
+           <h1> Practica 9</h1>
+          <div className='prueba'>Component1</div>
+    </div>
+  
+
   )
 }
 

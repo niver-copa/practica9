91271c6f90430b23dbd50a679b5d42e2835aba26
Mario <marioeduardo.garciasalazar@gmail.com>
2023-09-15 15:28:40 -0600

    Commit para componente 2

---
+++ practica9-react/componentes/Componente2.jsx
@@ -0,0 +1,9 @@
+function Componente2(){
+    return(
+        <>
+            <h1>Soy el componente 2</h1>
+        </>
+    )
+}
+
+export default Componente2
--- practica9-react/src/App.jsx
+++ practica9-react/src/App.jsx
@@ -1,10 +1,12 @@
 import Componente1 from '../componentes/Componente1'
+import Componente2 from '../componentes/Componente2'
 import './App.css'
 
 function App() {
   return (
     <>
       <Componente1 />
+      <Componente2 />
     </>
   )
 }

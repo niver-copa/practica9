6aa7f28fe85032371763212b96a596e4b145314a
Alonso <antalogm@gmail.com>
2023-09-14 18:18:26 -0600

    mensaje

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import { useState } from 'react'
 import Hola from './componet/Hola'
 import './App.css'
 import Imagen from './componet/Imagen'
+import Mensaje from './componet/Mensaje'
 
 function App() {
 
 
   return (
     <>
+    <div className='container'>
 <Hola/>
 <Imagen/>
-
+<Mensaje/>
+</div>
     </>
   )
 }

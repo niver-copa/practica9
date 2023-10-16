c599881aa1879ca193f7c849b4b1670a18783eb6
Uzielmf <Uziel.munozf@gmail.com>
2023-10-15 20:50:40 -0600

    se agrega el segundo componente

--- practica-9/src/components/Component-1.jsx
+++ practica-9/src/components/Component-1.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Component1() {
   return (
-    <div className='component1'>Hola soy el <br /> primer componente</div>
+    <div className='component uno'>Hola soy el <br /> primer componente</div>
   )
 }
 
---
+++ practica-9/src/components/Component-3.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Component3() {
+  return (
+    <div className='component tres'>Hola soy el <br /> tercer componente</div>
+  )
+}
+
+export default Component3

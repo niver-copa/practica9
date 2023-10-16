202da9f02dd1e3d047e9972b56aadad868a881c7
Uzielmf <Uziel.munozf@gmail.com>
2023-10-15 20:42:48 -0600

    se agrega el primer componente en el primer commit

---
+++ practica-9/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Component1 from './components/Component-1'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Component1></Component1>
+
+  )
+}
+
+export default App
